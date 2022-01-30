ebe95b5efeef1d015f4c6cb010da745d0612fa7f
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2022-01-29 20:00:53 -0600

    Updates

--- dashapp/App.py
+++ dashapp/App.py
@@ -1,19 +1,12 @@
-import os
-
 import dash
-from dash import dcc
-from dash import html
 import dash_bootstrap_components as dbc
 import flask
-from flask import request
 
-import base64
-from .Components import TextInput
+from .Components import DefaultLayout, TextInput
 from .utils import (
-    assign_ids_to_inputs, 
-    make_input_groups, 
-    make_output_groups, 
-    set_layout_skeleton, 
+    assign_ids_to_inputs,
+    make_input_groups,
+    make_output_groups,
     assign_ids_to_outputs
 )
 
@@ -40,10 +33,6 @@ class App(object):
         if outputs is None:
             self.outputs = [TextInput()]
 
-        self.output_state = [None] * len(self.outputs)
-
-        # self.output.id = 'output-component'
-
         self.title = title
         self.title_image_path = title_image_path
         self.subtext = subheader
@@ -51,14 +40,18 @@ class App(object):
         self.linkedin_url = linkedin_url
         self.twitter_url = twitter_url
 
+        # Assign IDs to components
         self.inputs_with_ids = assign_ids_to_inputs(
             self.inputs, self.callback_fn)
-
         self.outputs_with_ids = assign_ids_to_outputs(
             self.outputs)
 
-        server = flask.Flask(__name__)
+        # Default state of outputs
+        self.output_state_default = [
+            output_.placeholder for output_ in self.outputs_with_ids]
 
+        # Define Flask server
+        server = flask.Flask(__name__)
         external_stylesheets = [dbc.themes.YETI,
                                 "https://use.fontawesome.com/releases/v5.9.0/css/all.css"]
         self.app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
@@ -66,7 +59,7 @@ class App(object):
                              meta_tags=[{'name': 'viewport',
                                          'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.8, minimum-scale=0.5,'}]
                              )
-        # cache = Cache(self.app.server)
+        # Define app title
         self.app.title = title
 
         # Intialize layout
@@ -78,6 +71,7 @@ class App(object):
         # Keep track of the number of clicks
         self.submit_clicks = 0
         self.reset_clicks = 0
+        self.app_initialized = False
 
     def run(self):
         self.app.server.run(port=8080)  # int(os.environ.get('PORT', 8889)))
@@ -90,18 +84,21 @@ class App(object):
         if self.outputs is not None:
             output_groups = make_output_groups(self.outputs_with_ids)
 
-        self.app.layout = set_layout_skeleton(inputs=input_groups,
-                                              outputs=output_groups,
-                                              title=self.title,
-                                              title_image_path=self.title_image_path,
-                                              subtext=self.subtext,
-                                              github_url=self.github_url,
-                                              linkedin_url=self.linkedin_url,
-                                              twitter_url=self.twitter_url)
+        default_layout = DefaultLayout(inputs=input_groups,
+                                       outputs=output_groups,
+                                       title=self.title,
+                                       title_image_path=self.title_image_path,
+                                       subtext=self.subtext,
+                                       github_url=self.github_url,
+                                       linkedin_url=self.linkedin_url,
+                                       twitter_url=self.twitter_url)
+
+        self.app.layout = default_layout.layout
 
     def register_callback_fn(self):
         @self.app.callback(
-            [Output(component_id=output_.id, component_property=output_.attributable_property) for output_ in self.outputs_with_ids],
+            [Output(component_id=output_.id, component_property=output_.attributable_property)
+             for output_ in self.outputs_with_ids],
             [Input(component_id=input_.id, component_property=input_.attributable_property) for input_ in self.inputs_with_ids] +
             [Input(component_id='reset_inputs', component_property='n_clicks'),
              Input(component_id='submit_inputs', component_property='n_clicks')])
@@ -111,17 +108,24 @@ class App(object):
             submit_button = args[-1]
 
             if submit_button > self.submit_clicks:
+                self.app_initialized = True
                 self.submit_clicks = submit_button
                 output_state = self.callback_fn(*args[:-2])
+
                 if isinstance(output_state, tuple):
+                    self.output_state = list(output_state)
                     return output_state
 
-                return [self.output_state]
+                self.output_state = [output_state]
+                return self.output_state
 
             elif reset_button > self.reset_clicks:
                 self.reset_clicks = reset_button
-                self.output_state = [None]*len(self.outputs)
+                self.output_state = self.output_state_default
+                return self.output_state
+
+            elif self.app_initialized:
                 return self.output_state
 
             else:
-                return self.output_state
+                return self.output_state_default
--- dashapp/utils.py
+++ dashapp/utils.py
@@ -6,160 +6,7 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 
-def set_layout_skeleton(inputs=None,
-                        outputs=None,
-                        title=None,
-                        title_image_path=None,
-                        subtext=None,
-                        github_url=None,
-                        linkedin_url=None,
-                        twitter_url=None):
-
-    # 1. Navbar placeholder
-    social_media_navigation = []
-    if github_url is not None:
-        social_media_navigation.append(dbc.NavItem(dbc.NavLink(
-            html.I(className="fa-2x fab fa-github", style={'color': '#ffffff'}), href=github_url, target="_blank")))
-
-    if linkedin_url is not None:
-        social_media_navigation.append(dbc.NavItem(dbc.NavLink(html.I(
-            className="fa-2x fab fa-linkedin", style={'color': '#ffffff'}), href=linkedin_url, target="_blank")))
-
-    if twitter_url is not None:
-        social_media_navigation.append(dbc.NavItem(dbc.NavLink(html.I(
-            className="fa-2x fab fa-twitter-square", style={'color': '#ffffff'}), href=twitter_url, target="_blank")))
-
-    # 1. Navbar
-    navbar = dbc.NavbarSimple(
-        children=[dbc.NavItem(dbc.NavLink("About", href="#"))
-                  ] + social_media_navigation,
-        brand=title,
-        color="primary",
-        dark=True,
-        fluid=True,
-        fixed='top'
-    )
-
-    # 2. Top container
-    top_container_children = []
-
-    if title is not None:
-        top_container_children.append(
-            dbc.Row(
-                html.H1(title, style={'textAlign': 'center'}),
-                style={"padding": "8% 0% 2% 0%"}
-            )
-        )
-
-    if title_image_path is not None:
-        top_container_children.append(
-            dbc.Row(
-                dbc.Row(
-                    html.Img(src=title_image_path,
-                             style={
-                                 'width': '250px'
-                             }),
-                    justify='center'
-                )
-            )
-        )
-
-    if subtext is not None:
-        top_container_children.append(
-            dbc.Row(
-                dbc.Row(
-                    html.H4(
-                        html.I(subtext), style={'textAlign': 'center'}),
-                    style={"padding": "2% 0% 2% 0%"}
-                )
-            )
-        )
-
-    # 7. Display original and processed images
-    input_output_components = dbc.Row(
-        [
-            dbc.Col(inputs,
-                    style={"padding": "2% 1% 1% 2%",
-                           'lineHeight': '60px',
-                           'borderWidth': '1px',
-                           'borderRadius': '5px',
-                           "background-color": "#F2F2F2"},
-                    width=5,
-                    xs=12,
-                    sm=12,
-                    md=5,
-                    lg=5,
-                    xl=5,
-                    xxl=5),
-            dbc.Col(outputs,
-                    id="output-group",
-                    style={"padding": "2% 1% 1% 2%",
-                           'lineHeight': '60px',
-                           'borderWidth': '1px',
-                           'borderRadius': '5px',
-                           "background-color": "#F2F2F2"},
-                    width=5,
-                    xs=12,
-                    sm=12,
-                    md=5,
-                    lg=5,
-                    xl=5,
-                    xxl=5)
-        ],
-        justify='evenly',
-        style={"padding": "2% 1% 10% 2%"}
-    )
-
-    # 8. Footer
-    footer = dbc.NavbarSimple(
-        brand='Made with Dash',
-        color="primary",
-        dark=True,
-        fluid=True,
-        fixed='bottom'
-    )
-
-    # Bring it together
-    navbar_container = dbc.Container(
-        [
-            navbar
-        ],
-        fluid=True
-    )
-
-    top = dbc.Container(
-        top_container_children
-    )
-
-    middle = dbc.Container(
-        [
-            input_output_components
-        ],
-        fluid=True
-    )
-
-    bottom = dbc.Container(
-        [
-            footer
-        ],
-        fluid=True
-    )
-
-    layout = dbc.Container(
-        [
-            navbar_container,
-            top,
-            middle,
-            bottom
-        ],
-        fluid=True
-    )
-
-    return layout
-
-# Input utils
-
-
+# Input component utils
 def get_input_names_from_callback_fn(callback_fn):
     """
     Returns the names of function arguments as a list of strings
@@ -207,8 +54,6 @@ def make_input_groups(inputs_with_ids):
 
     button_row = html.Div(
         [
-            dbc.Button("Reset", outline=True, color="primary",
-                       className="me-1", id='reset_inputs', n_clicks=0),
             dbc.Button("Submit", color="warning", className="me-1",
                        id='submit_inputs', n_clicks=0)
         ]
@@ -239,8 +84,6 @@ def assign_ids_to_outputs(outputs):
 def make_output_groups(outputs):
 
     output_groups = []
-    n_outputs = len(outputs)
-
     output_groups.append(html.H2('Output'))
 
     for idx, output_ in enumerate(outputs):
@@ -255,5 +98,14 @@ def make_output_groups(outputs):
                 align='center'
             )
         )
+
+    button_row = html.Div(
+        [
+            dbc.Button("Clear", outline=True, color="primary",
+                       className="me-1", id='reset_inputs', n_clicks=0)
+        ]
+    )
+
+    output_groups.append(button_row)
 
     return output_groups
